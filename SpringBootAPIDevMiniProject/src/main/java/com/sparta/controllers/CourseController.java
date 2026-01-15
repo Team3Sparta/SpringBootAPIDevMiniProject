db909bd1f8fd42d93790b554b98f4970ba369fb7
spartamq <mjabeen@spartaglobal.com>
2026-01-15 11:54:41 +0000

    refactor dto and add exclusion by name repository functionality

--- SpringBootAPIDevMiniProject/src/main/java/com/sparta/controllers/CourseController.java
+++ SpringBootAPIDevMiniProject/src/main/java/com/sparta/controllers/CourseController.java
@@ -1,6 +1,6 @@
 package com.sparta.controllers;
 
-import com.sparta.dtos.CourseDto;
+import com.sparta.dtos.CourseDTO;
 import com.sparta.services.CourseService;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.http.ResponseEntity;
@@ -18,18 +18,18 @@ public class CourseController {
         this.service = service;
     }
 
-    @Operation(summary = "get all courses", description = "Get list of all vourses")
+    @Operation(summary = "get all courses", description = "Get list of all courses")
     @GetMapping(value = "/")
-    public ResponseEntity<List<CourseDto>> getAllEntities(){
-        List<CourseDto> books = service.getAllCourses();
+    public ResponseEntity<List<CourseDTO>> getAllEntities(){
+        List<CourseDTO> books = service.getAllCourses();
         return ResponseEntity.ok(books);
     }
 
     @Operation(summary = "Get a course by ID", description = "Retrieve a course from the database using their unique ID")
     @GetMapping(value = "/{id}")
-    public ResponseEntity<CourseDto> getEntityById(@PathVariable Integer id){
+    public ResponseEntity<CourseDTO> getEntityById(@PathVariable Integer id){
 
-        CourseDto c = this.service.getEntityByID(id);
+        CourseDTO c = this.service.getEntityByID(id);
 
         if(c!=null){
             return ResponseEntity.ok(c);
@@ -40,14 +40,21 @@ public class CourseController {
 
     @Operation(summary = "Add a new course", description = "Create a new course in the database")
     @PostMapping("/")
-    public ResponseEntity<CourseDto> addCourse(@RequestBody CourseDto entity){
+    public ResponseEntity<CourseDTO> addCourse(@RequestBody CourseDTO entity){
 
-        CourseDto savedEntity = service.saveCourse(entity);
+        CourseDTO savedEntity = service.saveCourse(entity);
         if(savedEntity != null){
             return ResponseEntity.ok(savedEntity);
         }else{
             return ResponseEntity.notFound().build();
         }
+    }
+
+    @Operation(summary = "get all courses that do not match the name", description = "Get list of all courses")
+    @GetMapping(params = "excludeName")
+    public ResponseEntity<List<CourseDTO>> getExcludedEntities(
+            @RequestParam("excludeName") String name    ) {
+        return ResponseEntity.ok(service.excludeCourse(name));
     }
 
 }
--- SpringBootAPIDevMiniProject/src/main/java/com/sparta/dtos/CourseMapper.java
+++ SpringBootAPIDevMiniProject/src/main/java/com/sparta/dtos/CourseMapper.java
@@ -5,6 +5,6 @@ import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface CourseMapper {
-    CourseDto toDTO(Course entity);
-    Course toEntity(CourseDto entityDto);
+    CourseDTO toDTO(Course entity);
+    Course toEntity(CourseDTO entityDto);
 }
