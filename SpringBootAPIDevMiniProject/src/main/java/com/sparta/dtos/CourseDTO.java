a96091d7d92480242351bf8088f60e1717f86af6
spartamq <mjabeen@spartaglobal.com>
2026-01-15 16:41:14 +0000

    fix exclude functionality

--- SpringBootAPIDevMiniProject/src/main/java/com/sparta/dtos/CourseDTO.java
+++ SpringBootAPIDevMiniProject/src/main/java/com/sparta/dtos/CourseDTO.java
@@ -1,13 +1,9 @@
 package com.sparta.dtos;
 
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-//import io.swagger.v3.oas.annotations.media.Schema;
 
 public class CourseDTO {
 
-    @JsonIgnore
-    //@Schema(hidden = true)
     private Integer id;
     private String courseName;
 
