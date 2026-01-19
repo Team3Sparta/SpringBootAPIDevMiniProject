a896471798256a9000efb4b93256bcbbdfe17143
MB <m.p.bialoszewski@outlook.com>
2026-01-19 16:35:06 +0000

    Revert "innit"
    This reverts commit 54d1d9336619d205b4c464d30c19069ed2c6fd97.

--- SpartaToDo/src/main/java/com/sparta/todo/spartatodo/repositories/TodoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sparta.todo.spartatodo.repositories;
-
-import com.sparta.todo.spartatodo.models.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TodoRepository extends JpaRepository<Todo, Integer> {
-}
